03831c30684ba02c294795a502219d2d6a2d7a6b
solangeachour <solangeachour@gmail.com>
2017-04-26 18:34:37 +0300

    new update:

---
+++ Exrecise/Es6/25-4-2017/app.js
@@ -0,0 +1,8 @@
+import React from"react";
+import ReactDom from "react-dom";
+class  Navbar extends React.Component {
+render() {
+    return (<p>Hello  </p>)
+}
+            }
+ ReactDom.REender(<Hello world/>, document.getElementById("app"));
